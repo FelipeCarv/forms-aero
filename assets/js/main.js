52cefc1eb18f61de55294bab1428a2273a637dd5
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-05-24 14:28:48 -0300

    adicionando a segunda página de prevencao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 
 import Dom from './modules/constructors.js'; //selecionar elementos
 import scriptsPrevencao1 from './modules/scriptsPaginas/prevencao1.js';
-
+import scriptsPrevencao0 from './modules/scriptsPaginas/prevencao0.js';
 
 // PAGES 
+
+const pagePrevencao0 = new Dom().el("#page__prevencao0")
+
+if (pagePrevencao0) { // ★ PREVENCAO 0
+    new Dom().bodyClass("body__prevencao0");
+    scriptsPrevencao0();
+}
+
+
 const pagePrevencao1 = new Dom().el("#page__prevencao1")
 
-if (pagePrevencao1) { // ★ HOME  
+if (pagePrevencao1) { // ★ PREVENCAO 1
     new Dom().bodyClass("body__prevencao1");
     scriptsPrevencao1();
 }
