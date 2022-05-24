9946b9f54eb91efbc9f9017461d23b6dbd12a0e7
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-05-24 15:57:06 -0300

    adicionando a primeira página de fadiga

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 import Dom from './modules/constructors.js'; //selecionar elementos
 import scriptsPrevencao1 from './modules/scriptsPaginas/prevencao1.js';
 import scriptsPrevencao0 from './modules/scriptsPaginas/prevencao0.js';
+import scriptsFadiga01 from './modules/scriptsPaginas/fadiga01.js';
 
 // PAGES 
 
@@ -18,6 +19,13 @@ const pagePrevencao1 = new Dom().el("#page__prevencao1")
 if (pagePrevencao1) { // ★ PREVENCAO 1
     new Dom().bodyClass("body__prevencao1");
     scriptsPrevencao1();
+}
+
+const pageFadiga01 = new Dom().el("#page__fadiga01")
+
+if (pageFadiga01) { // ★ FADIGA 1
+    new Dom().bodyClass("body__fadiga01");
+    scriptsFadiga01();
 }
 
 
