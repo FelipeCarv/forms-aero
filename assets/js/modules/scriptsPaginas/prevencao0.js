52b87e54977f3e117ec89e132e8a738c85fe641b
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-05-26 15:54:51 -0300

    realizando algumas correções

--- assets/js/modules/scriptsPaginas/prevencao0.js
+++ assets/js/modules/scriptsPaginas/prevencao0.js
@@ -1,3 +1,11 @@
 export default function scriptsPrevencao0(){
     // 'colocar scripts das respectivas paginas dentro de -scriptsPaginas-  e criar o arquivo exemplo: home.js - contato.js  --- e importar no main e executar em sua respectiva pagina '
+    const btnAbrir = document.getElementById('continuar');
+    // const btnFechar = document.getElementById('back')
+
+    btnAbrir.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const modal = document.getElementById('modal-planos');
+        modal.classList.add('mostrar');
+    });
 }
