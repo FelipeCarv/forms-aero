2d09f549479e512bdb826526a45fd5044d201614
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-05-30 11:23:40 -0300

    finalizando formulário

--- assets/js/modules/scriptsPaginas/fadiga03.js
+++ assets/js/modules/scriptsPaginas/fadiga03.js
@@ -1,3 +1,11 @@
 export default function scriptsFadiga03(){
     // 'colocar scripts das respectivas paginas dentro de -scriptsPaginas-  e criar o arquivo exemplo: home.js - contato.js  --- e importar no main e executar em sua respectiva pagina '
+
+    const btnAbrir = document.getElementById('adicionar');
+    
+    btnAbrir.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const ativar = document.getElementById('opcao1');
+        ativar.classList.add('ativar');
+    });
 }
